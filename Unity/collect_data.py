b46a2575d62e7c0455309378f76e5d9f2f6c48f9
mdalal2020 <mdalal@berkeley.edu>
2018-03-30 23:26:25 -0700

    updated collect data.py

--- Unity/collect_data.py
+++ Unity/collect_data.py
@@ -12,24 +12,24 @@ states_taken = []
 actions_taken = []
 threshold = 10
 
+# states = np.load("states.npy")
+# actions = np.load("actions.npy")
+# print('Num Batches Currently: ', states.shape[0])
 for i in range(max_trajectories):
     pause = input("press enter to start")
     done = False
     env.reset(train_mode=False)
-    count = 0
     episode_states = []
     episode_actions = []
     while not done:
-        count = (count + 1)
         brainInf = env.step()['DroneBrain']
         ob = brainInf.observations
         states = brainInf.states
         actions = brainInf.previous_actions[0][:2] #not saving the yaw
         norm = np.linalg.norm(states[0][3:6] - states[0][9:12])
         done = norm < threshold
-        if count % frequency == 0:
-            episode_states.append(states[0])
-            episode_actions.append(actions)
+        episode_states.append(states[0])
+        episode_actions.append(actions)
     save  = input('Save this trajectory(y/n): ') == 'y'
     if save:
         states_taken.extend(episode_states)
@@ -37,13 +37,10 @@ for i in range(max_trajectories):
     end = input('Stop collecting Data(y/n): ') == 'y'
     if end:
         break
+# states_taken.extend(np.ndarray.tolist(states))
+# actions_taken.extend(np.ndarray.tolist(actions))
 states_taken = np.array(states_taken)
 actions_taken = np.array(actions_taken)
 np.save("states", states_taken)
 np.save("actions", actions_taken)
-env.close()
-    # print(ob[0].shape)
-    # print(states)
-    # print(done)
-    # print(brainInf)
-    # print("--------------------------")
+# print('Num Batches After Data Collection: ', states_taken.shape[0])
--- Unity/dagger_drone.py
+++ Unity/dagger_drone.py
@@ -2,6 +2,7 @@ import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.optimizers import Adam
+from unityagents import UnityEnvironment
 
 #load data
 from sklearn.model_selection import train_test_split
@@ -18,17 +19,15 @@ model.add(Dense(128, input_shape=(13,)))
 model.add(Activation('relu'))
 model.add(Dense(64))
 model.add(Activation('relu'))
-model.add(Dense(32))
-model.add(Activation('relu'))
 model.add(Dense(2))
 
 model.compile(loss='mean_squared_error',
-              optimizer=Adam(lr=1e-3),
+              optimizer=Adam(lr=1e-4),
               metrics=['mean_squared_error'])
 
 num_epochs = 300
 
-batch_size = 256
+batch_size = 128
 model.fit(states_train,
           actions_train,
           validation_split=0.1,
@@ -39,4 +38,20 @@ print()
 print(model.metrics_names)
 print("Test error:", model.evaluate(states_test, actions_test))
 
+env = UnityEnvironment(file_name="drone_sim_external", worker_id=0)
+num_dagger_iterations = 10
+steps = 1000
+for iterations in range(num_dagger_iterations):
+    done = False
+    env.reset(train_mode=False)
+    states = np.zeros((1, 13))
+    threshold = 10
+    for i in range(steps):
+        action = model.predict(states)
+        action = np.hstack((action[0], 0))
+        brainInf = env.step(action)['DroneBrain']
+        states = brainInf.states
+        norm = np.linalg.norm(states[0][3:6] - states[0][9:12])
+
+        #TODO: FIGURE OUT HOW TO GENERATE LABELED ACTIONS
 model.save("trained_model.h5")
